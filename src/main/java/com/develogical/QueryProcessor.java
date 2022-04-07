ec988d992b8386f7229cdd786a121da8a359d5e0
JacobGirling98 <jacob.girling98@gmail.com>
2022-04-07 15:13:24 +0100

    find largest number

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,8 +1,14 @@
 package com.develogical;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class QueryProcessor {
 
     public String process(String query) {
+
+//        which of the following numbers is the largest: 2, 55, 520, 814
 
         System.out.println(query);
 
@@ -12,7 +18,18 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if(query.toLowerCase().contains("your name")) {
             return "CrypticFortress";
+        } else if (query.toLowerCase().contains("which of the following numbers is the largest")) {
+            return findLargestNumber(query);
         }
         return "";
+    }
+
+    private String findLargestNumber(String query) {
+        List<String> split = Arrays.asList(query.toLowerCase().split(":"));
+        List<Integer> numbers = Arrays.stream(split.get(1).split(","))
+                .map(num -> Integer.parseInt(num.trim()))
+                .sorted()
+                .collect(Collectors.toList());
+        return numbers.get(numbers.size() - 1 ).toString();
     }
 }
--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class QueryProcessorTest {
@@ -29,5 +30,11 @@ public class QueryProcessorTest {
     @Test
     public void canGetTeamName() {
         assertThat(queryProcessor.process("your name"), containsString("CrypticFortress"));
+    }
+
+    @Test
+    public void canGetLargestNumber() {
+        String query = "which of the following numbers is the largest: 2, 55, 520, 814";
+        assertEquals("814", queryProcessor.process(query));
     }
 }
